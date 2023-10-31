3fbbd5c6de3f7ede40f780b36e66d622b3feac36
Robert Mayo Elumba <126035170+RobertProCoder@users.noreply.github.com>
2023-10-31 23:06:08 +0800

    Remove About file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import "../css/navbar.css";
+import About from "./About";
+
 function Navbar() {
   return (
     <div className="Navbar">
       <h1 className="title">MAYO PIZZA</h1>
       <div className="menus">
-        <p onClick={console.log("Gi toplok")}>About</p>
+        <p>About</p>
         <p>Ingredients</p>
         <p>Contact</p>
       </div>
